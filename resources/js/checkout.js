729946f1a2de6b14b5b5b19bf10673aaab639dd8
knappm124 <118485646+knappm124@users.noreply.github.com>
2024-07-21 19:35:21 -0400

    Moved admin functions to pw protected folder, started checkout wireframe

---
+++ resources/js/checkout.js
@@ -0,0 +1,13 @@
+function showCheckoutForm() {
+
+}
+
+function showCheckedOutBooks() {
+
+}
+
+function hideCheckout() {
+    document.queryselector('#checkoutForm').ClassList.add('hidden');
+    document.queryselector('#checkedOutBooks').ClassList.add('hidden');
+    document.queryselector('#checkoutAdminForms').ClassList.add('hidden');
+}
