24d1b76f68e7c3f6526227e4f1a666209b4eaa4e
Melissa Knapp <knappm124@gmail.com>
2024-07-22 12:06:04 -0400

    updated paths for admin folder files, layed out wireframe for checkout

--- resources/js/checkout.js
+++ resources/js/checkout.js
@@ -1,13 +1,28 @@
 function showCheckoutForm() {
-
+    document.querySelector('#checkoutAdminForm').classList.add('hidden');
+    document.querySelector('#checkOutBookButton').classList.add('hidden');
+    document.querySelector('#checkoutForm').classList.remove('hidden');
+    document.querySelector('#goBackButton').classList.remove('hidden');
 }
 
 function showCheckedOutBooks() {
-
+    document.querySelector('#checkedOutBooksButton').classList.add('hidden');
+    document.querySelector('#checkedOutBooks').classList.remove('hidden');
+    document.querySelector('#goBackButton').classList.add('hidden');
+    document.querySelector('#goBackButton2').classList.remove('hidden');
 }
 
 function hideCheckout() {
-    document.queryselector('#checkoutForm').ClassList.add('hidden');
-    document.queryselector('#checkedOutBooks').ClassList.add('hidden');
-    document.queryselector('#checkoutAdminForms').ClassList.add('hidden');
+    document.querySelector('#checkoutAdminForm').classList.remove('hidden');
+    document.querySelector('#checkOutBookButton').classList.remove('hidden');
+    document.querySelector('#checkoutForm').classList.add('hidden');
+    document.querySelector('#goBackButton').classList.add('hidden');
+}
+
+function hideCheckedOutBooks() {
+    document.querySelector('#checkedOutBooksButton').classList.remove('hidden');
+    document.querySelector('#checkedOutBooks').classList.add('hidden');
+    document.querySelector('#goBackButton').classList.remove('hidden');
+    document.querySelector('#goBackButton2').classList.add('hidden');
+
 }
